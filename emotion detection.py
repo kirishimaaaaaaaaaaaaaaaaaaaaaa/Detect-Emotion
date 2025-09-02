6db5e435cadea6b978f288b92f6ecf4e8e3d8b4a
gigmaaaa <166723136+kirishimaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2025-09-02 15:30:43 +0545

    Update emotion detection.py

--- emotion detection.py
+++ emotion detection.py
@@ -1,38 +1,31 @@
 import cv2
-import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array
+from deepface import DeepFace
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-model = load_model("src/emotion_model.h5")
-emotion_labels = ['Angry', 'Happy', 'Sad', 'Surprise', 'Neutral']
+roi_window = cv2.namedWindow('ROI Select', cv2.WINDOW_NORMAL)
+cv2.setWindowProperty('ROI Select', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+face_classifier = cv2.CascadeClassifier()
+face_classifier.load(cv2.samples.findFile(r"C:\Users\Lenovo\Downloads\haarcascade_frontalface_default.xml"))
 
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-    if not ret:
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(frame_gray)
+    response = DeepFace.analyze(frame, actions=("emotion"), enforce_detection=False)
+
+    for face in faces:
+        x, y, w, h = face
+        # print(x,y)
+        newframe = cv2.rectangle(frame, (x,y), (x+w,y+h), color=(0,0,255), thickness=2)
+        cv2.putText(frame, text = response[0]["dominant_emotion"], org=(x,y), fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1, color=(0,0,255))
+    cv2.namedWindow("emotion detection",cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("emotion detection",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+    cv2.putText(frame, text = "Press Q key to exit", org=(10,470), fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1, color=(0,0,255))
+    cv2.imshow("emotion detection",frame)
+    if cv2.waitKey(1) == ord("q"):
         break
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-    for (x, y, w, h) in faces:
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_gray = cv2.resize(roi_gray, (48, 48))
-        roi_gray = roi_gray.astype("float") / 255.0
-        roi_gray = img_to_array(roi_gray)
-        roi_gray = np.expand_dims(roi_gray, axis=0)
-
-        preds = model.predict(roi_gray)[0]
-        label = emotion_labels[np.argmax(preds)]
-
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        cv2.putText(frame, label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-    cv2.imshow("Face & Emotion Detection", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
 cap.release()
 cv2.destroyAllWindows()
